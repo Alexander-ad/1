95abd36977139f8345064af9ff8d8693fdf2b084
Alexander-ad <88938603+Alexander-ad@users.noreply.github.com>
2022-02-14 20:26:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '5169322523:AAF3BN9E5-6y24qXTtLjoxs6CXId9h-sWYI'
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+class CurrencyConverter:
+    @staticmethod
+    def convert(base: str, quote: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = json.loads(r.content)[keys[quote]]
+        total_base = total_base * amount
+
+        return round(total_base, 2)
